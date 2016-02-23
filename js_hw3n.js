f056260d8016fe33beae60095bc7ff2cdbcd7cea
Igor Romanov <romaigoroleg@gmail.com>
2016-02-23 18:26:36 +0200

    sasha comments

--- js_hw3n.js
+++ js_hw3n.js
@@ -37,3 +37,18 @@ function todaysDate() {
 today = dd + '.' + mm + '.' + yyyy;
 return today;
 }
+
+// sasha comments
+function test(){
+var today = new Date();
+WScript.Echo(today.getMinutes() + ":" + today.getMilliseconds());
+var otherDate = new Date(2016, 1, 24);
+WScript.Echo(otherDate.getMinutes() + ":" + otherDate.getMilliseconds());
+if (today > otherDate) {
+WScript.Echo("today >: " + formatdate(otherDate, "."));
+}
+else
+{
+WScript.Echo("today <: " + formatdate(otherDate, ".") + " today: " +formatdate(today, "."));
+}
+}
